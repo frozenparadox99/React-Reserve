a6cd616f939ee90a4470c6249b8314a4e7aa5368
frozenparadox99 <frostbitecryengine300@gmail.com>
2020-04-15 22:24:44 +0530

    fetching data with getInitialProps

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,29 @@
-function Home() {
+import React, { useEffect } from "react";
+import axios from "axios";
+
+function Home({ products }) {
+  console.log(products);
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  async function getProducts() {
+    const url = "http://localhost:3000/api/products";
+    const response = await axios.get(url);
+    // console.log(response.data);
+  }
+
   return <>home</>;
 }
+
+Home.getInitialProps = async () => {
+  // Fetch data from the server
+  const url = "http://localhost:3000/api/products";
+  const response = await axios.get(url);
+
+  //Return data as an object
+  return { products: response.data };
+  // note: This object is merged with existing props
+};
 
 export default Home;
