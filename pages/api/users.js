3d57d288483d066c3c3e68988a99711bebd4bbb1
frozenparadox99 <frostbitecryengine300@gmail.com>
2020-04-21 20:28:28 +0530

    formatted dates

--- pages/api/users.js
+++ pages/api/users.js
@@ -10,7 +10,9 @@ export default async (req, res) => {
       req.headers.authorization,
       process.env.JWT_SECRET
     );
-    const users = await User.find({ _id: { $ne: userId } });
+    const users = await User.find({ _id: { $ne: userId } }).sort({
+      role: "asc",
+    });
     console.log(users);
     res.status(200).json(users);
   } catch (error) {
