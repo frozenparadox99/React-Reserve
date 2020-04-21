a8f4cc711a9bf3dc350e19e3d3cc071f8e45f263
frozenparadox99 <frostbitecryengine300@gmail.com>
2020-04-21 18:05:22 +0530

    added pagination to product list

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,31 @@
 import React, { useEffect } from "react";
 import axios from "axios";
 import ProductList from "../components/Index/ProductList";
+import ProductPagination from "../components/Index/ProductPagination";
 import baseUrl from "../utils/baseUrl";
 
-function Home({ products }) {
-  return <ProductList products={products} />;
+function Home({ products, totalPages }) {
+  return (
+    <>
+      <ProductList products={products} />
+      <ProductPagination totalPages={totalPages} />
+    </>
+  );
 }
 
-Home.getInitialProps = async () => {
+Home.getInitialProps = async (ctx) => {
+  const page = ctx.query.page ? ctx.query.page : "1";
+  const size = 9;
+  console.log(ctx.query);
   // Fetch data from the server
   const url = `${baseUrl}/api/products`;
-  const response = await axios.get(url);
+  const payload = {
+    params: { page, size },
+  };
+  const response = await axios.get(url, payload);
 
   //Return data as an object
-  return { products: response.data };
+  return response.data;
   // note: This object is merged with existing props
 };
 
